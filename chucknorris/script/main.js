85356ebe635aa931d27dd939c52778977a0e3f9c
vallejoanderson <vallejoanderson@gmail.com>
2021-08-15 11:06:32 -0500

    Using chucknorrisjoke api

--- chucknorris/script/main.js
+++ chucknorris/script/main.js
@@ -1,9 +1,17 @@
+const text = document.querySelector('.quote');
+const btn = document.getElementById('button');
+
 let requestURL = "https://api.chucknorris.io/jokes/random";
 let request = new XMLHttpRequest();
-request.open('GET', requestURL);
-request.responseType = "json";
-request.send();
-request.onload = function() {
-    const quote = request.response;
-    console.log(quote);
+btn.addEventListener('click', function(){
+    request.open('GET', requestURL);
+    request.responseType = "json";
+    //request.responseType= "text";
+    request.onload = function() {
+        const quote = request.response;
+        //console.log(JSON.parse(quote));
+        text.textContent = quote.value;
+    }
+    request.send();
 }
+);
